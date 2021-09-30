d38b5932215110d6513325c5f1f8c5597e293db5
Meahgan Shanks <meahganshanks@Meahgans-Air.localdomain>
2021-09-29 21:43:06 -0400

    make active and listener added

--- js/app.js
+++ js/app.js
@@ -1,73 +1,73 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 const navbarList = document.getElementById("navbar__list");
+const sections = document.getElementsByTagName("Section");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
+function makeActive() {
+  for (const section of sections) {
+    const box = section.getBoundingClientRect();
+    const activeListItem = document.getElementById("menu__item__" + section.id);
 
+    if (box.top <= 150 && box.bottom >= 150) {
+      // Apply active state on the current section and the corresponding Nav link.
+      section.className = "active";
+      activeListItem.className = "active";
+    } else {
+      // Remove active state from other section and corresponding Nav link.
+      section.className = "";
+      activeListItem.className = "";
+    }
+  }
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-const sections = document.getElementsByTagName("Section");
 for (let i = 0; i < sections.length; i++) {
-    const section = sections[i];
+  const section = sections[i];
 
-    let anchor = document.createElement("a");
-    anchor.className = "menu__link";
-    anchor.textContent = section.getAttribute("data-nav");
-    anchor.href = "#" + section.id
+  let anchor = document.createElement("a");
+  anchor.className = "menu__link";
+  anchor.textContent = section.getAttribute("data-nav");
+  anchor.href = "#" + section.id;
 
-    let listItem  = document.createElement("li");
-    listItem.append(anchor);
-    navbarList.append(listItem);
-
+  let listItem = document.createElement("li");
+  listItem.id = "menu__item__" + section.id;
+  listItem.append(anchor);
+  navbarList.append(listItem);
 }
-
-
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+document.addEventListener("scroll", makeActive);
