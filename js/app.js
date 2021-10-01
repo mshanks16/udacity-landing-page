749813f872ff630a0253f8d5556d1ec538491b3c
Meahgan Shanks <absalomm16@gmail.com>
2021-10-01 14:53:49 -0400

    fix scrollTo

--- js/app.js
+++ js/app.js
@@ -29,18 +29,25 @@ const sections = document.getElementsByTagName("Section");
 function makeActive() {
   for (const section of sections) {
     const box = section.getBoundingClientRect();
-    const activeListItem = document.getElementById("menu__item__" + section.id);
+    const activeAnchor = document.getElementById("menu__item__" + section.id);
 
     if (box.top <= 150 && box.bottom >= 150) {
       // Apply active state on the current section and the corresponding Nav link.
       section.className = "active";
-      activeListItem.className = "active";
+      activeAnchor.className = "menu__link active";
     } else {
       // Remove active state from other section and corresponding Nav link.
       section.className = "";
-      activeListItem.className = "";
+      activeAnchor.className = "menu__link";
     }
   }
+}
+
+function scrollTo(event) {
+  event.preventDefault()
+  const sectionId = event.target.id.replace("menu__item__", "");
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView();
 }
 
 /**
@@ -56,10 +63,12 @@ for (let i = 0; i < sections.length; i++) {
   let anchor = document.createElement("a");
   anchor.className = "menu__link";
   anchor.textContent = section.getAttribute("data-nav");
-  anchor.href = "#" + section.id;
+  anchor.id = "menu__item__" + section.id;
+  anchor.href = "#";
+  anchor.addEventListener("click", scrollTo);
+
 
   let listItem = document.createElement("li");
-  listItem.id = "menu__item__" + section.id;
   listItem.append(anchor);
   navbarList.append(listItem);
 }
